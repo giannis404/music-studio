6864b27da25080d5dde4108ce4edabb879696bb8
giannis404 <giannis404@yahoo.gr>
2025-09-07 01:06:53 +0200

    another try

---
+++ frontend/pages/preview/disable.vue
@@ -0,0 +1,10 @@
+<script setup>
+const previewCookie = useCookie("__sanity_preview");
+previewCookie.value = null;
+
+await navigateTo("/");
+</script>
+
+<template>
+  <div>Disabling preview mode...</div>
+</template>
---
+++ frontend/pages/preview/enable.vue
@@ -0,0 +1,19 @@
+<script setup>
+const route = useRoute();
+const pathname = route.query["sanity-preview-pathname"] || "/";
+
+// Set preview cookie
+const previewCookie = useCookie("__sanity_preview", {
+  maxAge: 60 * 60 * 24,
+  sameSite: "lax",
+});
+
+previewCookie.value = "true";
+
+// Redirect to the target page
+await navigateTo(pathname);
+</script>
+
+<template>
+  <div>Enabling preview mode...</div>
+</template>
