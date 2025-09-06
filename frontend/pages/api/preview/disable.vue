7f1cd19fdf02dbd1f9137f415486a101b04ef731
giannis404 <giannis404@yahoo.gr>
2025-09-06 23:20:27 +0200

    more trials

---
+++ frontend/pages/api/preview/disable.vue
@@ -0,0 +1,12 @@
+<script setup>
+// Clear preview cookie
+const previewCookie = useCookie("__sanity_preview");
+previewCookie.value = null;
+
+// Redirect to home
+await navigateTo("/");
+</script>
+
+<template>
+  <div>Disabling preview...</div>
+</template>
