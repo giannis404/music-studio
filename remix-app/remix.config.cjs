ae95000c7dc85503e56a96dca1dbd61713f8d24a
giannis404 <giannis404@yahoo.gr>
2025-09-07 04:08:24 +0200

    more remix settings

--- remix-app/remix.config.cjs
+++ remix-app/remix.config.cjs
@@ -1,6 +1,9 @@
+const {netlifyPlugin} = require('@netlify/remix-adapter/plugin')
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
+  plugins: [netlifyPlugin()],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
